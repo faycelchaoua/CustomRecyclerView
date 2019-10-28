d09fc3dd163a96bef812daa352cb617df1f0390f
Faycel CHAOUA <faycelchaoua@gmail.com>
2019-10-28 18:48:22 +0100

    Initial commit

---
+++ app/src/main/java/com/isetkelibia/customrecyclerview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.isetkelibia.customrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/isetkelibia/customrecyclerview/RecyclerAdapter.java
@@ -0,0 +1,73 @@
+package com.isetkelibia.customrecyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private String[] titles = {"Word",
+            "Excel",
+            "PowerPoint",
+            "Outlook"};
+    private String[] details = {"Editeur de texte",
+            "Tableur",
+            "Logiciel de présentation",
+            "Client de courrier électronique"};
+    private int[] images = {R.drawable.word,
+            R.drawable.excel,
+            R.drawable.powerpoint,
+            R.drawable.outlook};
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView itemImage;
+        private TextView itemTitle;
+        private TextView itemDetail;
+
+        private ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemImage = itemView.findViewById(R.id.item_image);
+            itemTitle = itemView.findViewById(R.id.item_title);
+            itemDetail = itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, titles[position] + " : " + details[position],
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.itemTitle.setText(titles[position]);
+        holder.itemDetail.setText(details[position]);
+        holder.itemImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+}
